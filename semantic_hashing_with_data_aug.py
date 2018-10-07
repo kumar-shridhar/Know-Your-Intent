848f524824efd97cbcfc1f4909c019c514f786d0
Kumar Shridhar <shridhar.stark@gmail.com>
2018-10-07 21:32:37 +0200

    Removed an extra 'j' from augmentation.

--- semantic_hashing_with_data_aug.py
+++ semantic_hashing_with_data_aug.py
@@ -87,8 +87,8 @@ class MeraDataset():
                                    's': {'x': 1, 'y': 1}, 'x': {'x': 1, 'y': 2}, 'd': {'x': 2, 'y': 1},
                                    'c': {'x': 2, 'y': 2}, 'f': {'x': 3, 'y': 1}, 'b': {'x': 4, 'y': 2},
                                    'm': {'x': 5, 'y': 2}, 'j': {'x': 6, 'y': 1}, 'g': {'x': 4, 'y': 1},
-                                   'h': {'x': 5, 'y': 1}, 'j': {'x': 6, 'y': 1}, 'k': {'x': 7, 'y': 1},
-                                   'l': {'x': 8, 'y': 1}, 'v': {'x': 3, 'y': 2}, 'n': {'x': 5, 'y': 2}, }
+                                   'h': {'x': 5, 'y': 1}, 'k': {'x': 7, 'y': 1}, 'n': {'x': 5, 'y': 2},
+                                   'l': {'x': 8, 'y': 1}, 'v': {'x': 3, 'y': 2}, }
         self.nearest_to_i = self.get_nearest_to_i(self.keyboard_cartesian)
         self.splits = self.stratified_split()
 
